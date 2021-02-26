ce74d21e431958c1acc95a7c63df3c645a4bbf05
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2021-02-26 18:50:15 +0200

    Ignore cypress in eslint

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,12 +1,12 @@
-describe('WebKitsas', function() {
-  it('front page can be opened', function() {
-    cy.visit('http://localhost:5050')
-    cy.contains('Tervetuloa WebKitsaaseen')
+describe("WebKitsas", function() {
+  it("front page can be opened", function() {
+    cy.visit("http://localhost:5050")
+    cy.contains("Tervetuloa WebKitsaaseen")
   })
 
-  it('Change password dialog', function() {
-    cy.visit('http://localhost:5050')
-    cy.contains('Unohdin salasanan').click()
-    cy.contains('Unohtuneen salasanan vaihtaminen')
+  it("Change password dialog", function() {
+    cy.visit("http://localhost:5050")
+    cy.contains("Unohdin salasanan").click()
+    cy.contains("Unohtuneen salasanan vaihtaminen")
   })
 })
